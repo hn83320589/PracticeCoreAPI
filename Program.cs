40fc2ccfc60781b089c43697c4c7cedc1cb78fcc
DADA <47731269+hn83320589@users.noreply.github.com>
2025-03-31 11:24:03 +0800

    feat: 更新套件版本並調整專案設定
    - 更新 MiniProfiler.AspNetCore.Mvc 至 4.5.4
    - 更新 Swashbuckle.AspNetCore.Annotations 和 Swashbuckle.AspNetCore.Newtonsoft 至 8.0.0
    - 調整 HTTP 伺服器埠號至 5281
    - 移除 Serilog 設定

--- Program.cs
+++ Program.cs
@@ -1,50 +1,31 @@
-using FluentValidation;
-using FluentValidation.AspNetCore;
+// 加入必要的命名空間引用
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
-using Serilog;
-using System.Collections.Generic;
-using Hangfire;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System.Text.Json;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // 將服務加入到容器
 builder.Services.AddHttpClient();
 
-// 修正後的 FluentValidation 設定方式
-builder.Services.AddControllers()
-    .AddNewtonsoftJson(options =>
-    {
-        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-        options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
-    });
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+    options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+});
 
 // 正確使用 FluentValidation 擴充方法
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddFluentValidationClientsideAdapters();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
-// 註冊 AutoMapper，掃描包含映射設定檔的組件
-builder.Services.AddAutoMapper(typeof(Program));
-
-// 了解更多有關設定 Swagger/OpenAPI，請參考：https://aka.ms/aspnetcore/swashbuckle
+// 了解更多關於設定 Swagger/OpenAPI 的資訊：https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// 設定 Serilog
-builder.Host.UseSerilog((context, configuration) => 
-    configuration
-        .ReadFrom.Configuration(context.Configuration)
-        .WriteTo.Console()
-        .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day));
 
 var app = builder.Build();
 

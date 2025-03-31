9c2025d381727d5458665abbef1c28d6e0c1ac88
DADA <47731269+hn83320589@users.noreply.github.com>
2025-03-31 10:39:47 +0800

    feat: 更新專案至 .NET 8 並新增多個套件依賴
    - 將目標框架更新為 .NET 8.0
    - 新增多個套件參考，包括 FluentValidation、AutoMapper、Serilog 等
    - 更新 HTTP 主機地址
    - 調整啟動設定以使用 Serilog 記錄

--- Program.cs
+++ Program.cs
@@ -1,17 +1,58 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Serilog;
+using System.Collections.Generic;
+using Hangfire;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 將服務加入到容器
+builder.Services.AddHttpClient();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// 修正後的 FluentValidation 設定方式
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+        options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+    });
+
+// 正確使用 FluentValidation 擴充方法
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+// 註冊 AutoMapper，掃描包含映射設定檔的組件
+builder.Services.AddAutoMapper(typeof(Program));
+
+// 了解更多有關設定 Swagger/OpenAPI，請參考：https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 設定 Serilog
+builder.Host.UseSerilog((context, configuration) => 
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .WriteTo.Console()
+        .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 設定 HTTP 請求管線
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
